043629e7b28b26bd07fc8ee183eae7ba4bf57a94
Weijian Wang <61218080+Stephen1202-Wang@users.noreply.github.com>
2023-12-14 11:15:52 -0800

    upload the v1.3 version of the scCDC

--- inst/doc/scCDC.R
+++ inst/doc/scCDC.R
@@ -1,23 +1,45 @@
 ## ----echo=FALSE, include=FALSE------------------------------------------------
-knitr::opts_chunk$set(tidy = FALSE, cache = TRUE, dev = "png",
-                      message = FALSE, error = FALSE, warning = FALSE)
+library(knitr)
+knitr::opts_chunk$set(tidy = FALSE, dev = "png",
+                      message = FALSE, warning = FALSE)
 
-## ----setup--------------------------------------------------------------------
+## ----setup,echo=FALSE---------------------------------------------------------
 suppressPackageStartupMessages(library(scCDC))
 suppressPackageStartupMessages(library(Seurat))
 suppressPackageStartupMessages(library(ggplot2))
 suppressPackageStartupMessages(library(tidyr))
-suppressPackageStartupMessages(library(pdftools))
 suppressPackageStartupMessages(library(ddpcr))
+
+## ----Quick_start,eval=FALSE---------------------------------------------------
+#  library(devtools)
+#  install_github("ZJU-UoE-CCW-LAB/scCDC")
+#  library(scCDC)
+#  # load data
+#  seuratobject <- readRDS('/path/to/seuratobject')
+#  # detect global contamination causing genes(GCGs)
+#  GCGs <- ContaminationDetection(seuratobject)
+#  # remove the contamination
+#  seuratobj_corrected <- ContaminationCorrection(seuratobject,rownames(GCGs))
+#  DefaultAssay(seuratobj_corrected) <- "Corrected"
+
+## ----eval=FALSE---------------------------------------------------------------
+#  corrected_count_matrix <- data.frame(seuratobj_corrected@assays[["Corrected"]]@counts)
+
+## ----eval=FALSE---------------------------------------------------------------
+#  if(!require("devtools")){
+#    install.packages("devtools")
+#  }
+#  library(devtools)
+#  install_github("ZJU-UoE-CCW-LAB/scCDC")
+
+## -----------------------------------------------------------------------------
+library(scCDC)
 
 ## ----load-data----------------------------------------------------------------
 data(mislet_before, package = "scCDC")
-class(mislet_before)
-summary(mislet_annotation)
 
 ## ----apply-Seurat,message=FALSE,warning=FALSE---------------------------------
 mislet_seuratobj<-CreateSeuratObject(mislet_before)
-mislet_seuratobj@active.ident<-mislet_annotation
 mislet_seuratobj <- NormalizeData(mislet_seuratobj,
                                   normalization.method = "LogNormalize",scale.factor = 10000)
 mislet_seuratobj <- FindVariableFeatures(mislet_seuratobj, 
@@ -26,50 +48,48 @@ all.genes <- rownames(mislet_seuratobj)
 mislet_seuratobj <- ScaleData(mislet_seuratobj, features = all.genes)
 mislet_seuratobj <- RunPCA(mislet_seuratobj, 
                                 features = VariableFeatures(object = mislet_seuratobj))
-
+# We use pre-defined clustering annotation here
+mislet_seuratobj@active.ident<-mislet_annotation
 mislet_seuratobj <- RunUMAP(mislet_seuratobj,dims=1:20)
 
-## ----ContaminationDetection.1-------------------------------------------------
-contamination <- ContaminationDetection(mislet_seuratobj,restriction_factor = 0.5, 
-                                        sample_name = "mislet",out_path.plot = "./",
-                                        out_path.table = "./")
-rownames(contamination)
+## ----ContaminationDetection.0-------------------------------------------------
+GCGs <- ContaminationDetection(mislet_seuratobj)
+rownames(GCGs)
 
-## ----ContaminationDetection.2,fig.width=1,fig.height=1------------------------
-quiet(pdf_convert("./mislet_SE-plot.pdf",format='jpg'))
-knitr::include_graphics("mislet_SE-plot_1.jpg")
-knitr::include_graphics("mislet_SE-plot_2.jpg")
-knitr::include_graphics("mislet_SE-plot_3.jpg")
-knitr::include_graphics("mislet_SE-plot_4.jpg")
+## ----ContaminationDetection.1, eval=FALSE-------------------------------------
+#  GCGs <- ContaminationDetection(mislet_seuratobj,restriction_factor = 0.5,
+#                                          sample_name = "mislet",out_path.plot = "./",
+#                                          out_path.table = "./")
 
-## ----ContaminationDetection.3-------------------------------------------------
-cont_table <- read.csv("mislet_degree_of_contamination.csv")
-cont_table_long <- tidyr::gather(cont_table, group, entropy_divergence, 
-                                 Alpha, Beta, Delta, Endothelial, mean_distance)
-cont_table_long_selected <- cbind(cont_table_long, 
-                                  type = rep("uncontaminated", length(cont_table_long[,1])))
-cont_table_long_selected[which(cont_table_long_selected$X %in% 
-                                 rownames(contamination)),4] <- "contaminated"
-ggplot(cont_table_long_selected) + geom_boxplot(aes(type,entropy_divergence),outlier.shape = NA) + 
-  facet_wrap(~group)
-
+## -----------------------------------------------------------------------------
+mislet_index <- ContaminationQuantification(mislet_seuratobj,rownames(GCGs))
+mislet_index
 
 ## ----ContaminationCorrection.1------------------------------------------------
-mislet_seuratobj_corrected <- ContaminationCorrection(mislet_seuratobj, 
-                                                      cont_genes = rownames(contamination),
-                                                      auc_thres = 0.9)
+mislet_seuratobj_corrected <- ContaminationCorrection(mislet_seuratobj, rownames(GCGs))
+
+## ----eval=FALSE---------------------------------------------------------------
+#  corrected_count_matrix = data.frame(mislet_seuratobj_corrected@assays[["Corrected"]]@counts)
 
 ## ----ContaminationCorrection.2------------------------------------------------
 DefaultAssay(mislet_seuratobj_corrected) <- "Corrected"
-mislet_seuratobj_corrected <- NormalizeData(mislet_seuratobj_corrected,assay = "Corrected",
+
+## ----ContaminationCorrection.3,result='hide',fig.show='hide'------------------
+mislet_seuratobj_corrected <- NormalizeData(mislet_seuratobj_corrected,
                                   normalization.method = "LogNormalize",scale.factor = 10000)
-mislet_seuratobj_corrected <- FindVariableFeatures(mislet_seuratobj_corrected,assay = "Corrected",
+mislet_seuratobj_corrected <- FindVariableFeatures(mislet_seuratobj_corrected,
                                                    selection.method = "vst", nfeatures = 2000)
 all.genes <- rownames(mislet_seuratobj_corrected)
-mislet_seuratobj_corrected <- ScaleData(mislet_seuratobj_corrected,assay = "Corrected",
+mislet_seuratobj_corrected <- ScaleData(mislet_seuratobj_corrected,
                                         features = all.genes)
+mislet_seuratobj_corrected <- RunPCA(mislet_seuratobj_corrected, 
+                                features = VariableFeatures(object = mislet_seuratobj_corrected))
+ElbowPlot(mislet_seuratobj_corrected)
+mislet_seuratobj_corrected <- FindNeighbors(mislet_seuratobj_corrected, dims = 1:15)
+mislet_seuratobj_corrected <- FindClusters(mislet_seuratobj_corrected, resolution = 0.2,verbose = F)
+mislet_seuratobj_corrected <- RunUMAP(mislet_seuratobj_corrected,dims=1:15)
 
-## ----ContaminationCorrection.3------------------------------------------------
+## ----ContaminationCorrection.4------------------------------------------------
 FeaturePlot(mislet_seuratobj, features = c("Ins1", "Gcg"))
 FeaturePlot(mislet_seuratobj_corrected, features = c("Ins1", "Gcg"))
 
