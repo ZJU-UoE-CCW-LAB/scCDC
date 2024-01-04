1e46e29ee0a9b43dbd028319b3f1905ab45dcc4c
Weijian Wang <61218080+Stephen1202-Wang@users.noreply.github.com>
2024-01-03 20:29:20 -0800

    update contamination quantification

--- R/Contamination_Quantification.R
+++ R/Contamination_Quantification.R
@@ -7,9 +7,9 @@
 #' @param gene a gene within the input SeuratObject
 #' @param eGCG_aucs a named vector of the AUROC values between the each cluster and the cluster with the lowest expression level
 #' @param auc_thres the AUROC threshold to determine the boundary between eGCG_positive and eGCG_negative clusters (Default as 0.9, 90 percent)
-#' @param slot the slot used for calculating contamination index
+#' @param slot the slot used for calculating contamination ratio
 #' 
-#' @return an index representing the contamination level calculated using the input gene
+#' @return an ratio representing the contamination level calculated using the input gene
 #'
 #' @import Seurat 
 #'
@@ -35,24 +35,24 @@ Cal_Cont_level <- function(object,gene,eGCG_aucs,auc_thres,slot){
   eGCG_neg_cells_exps<-obj_exp[gene,eGCG_neg_cells]
   eGCG_pos_cells_exps<-obj_exp[gene,eGCG_pos_cells]
   
-  cont_index = log10(sum(eGCG_neg_cells_exps)/sum(obj_exp[,eGCG_neg_cells]))
+  cont_index = sum(eGCG_neg_cells_exps)/sum(obj_exp[,eGCG_neg_cells])
   
   return(cont_index)
 }
 
 
-#' Calculate contamination index for a dataset
+#' Calculate contamination ratio for a dataset
 #'
-#' Calculating the contamination index for a dataset leveraging GCGs. For each GCG, the contamination level is calculated by dividing its total expression in eGCG- cells by 
-#' the total expression of all genes in the eGCG- cells. The maximum of contamination index is returned. Higher contamination index represents higher contamination level.
+#' Calculating the contamination ratio for a dataset leveraging GCGs. For each GCG, the contamination level is calculated by dividing its total expression in eGCG- cells by 
+#' the total expression of all genes in the eGCG- cells. The maximum of contamination ratio is returned. Higher contamination ratio represents higher contamination level.
 #'
 #' @param object a clustered SeuratObject
 #' @param cont_genes a contaminative geneset within the input SeuratObject
 #' @param min.cell the parameter used to filter the cell populations without sufficient number of cells. Cell populations that reaches the threshold could be used in downstream analysis.
-#' @param top10_gcg a parameter controlling whether to use the top 10 GCGs for calculating contamination index
-#' @param slot the slot used for calculating contamination index
+#' @param top10_gcg a parameter controlling whether to use the top 10 GCGs for calculating contamination ratio
+#' @param slot the slot used for calculating contamination ratio
 #' 
-#' @return the maximum value of the calculated contamination index
+#' @return the maximum value of the calculated contamination ratio
 #'
 #' @import Seurat pROC ddpcr pbapply
 #' @export
@@ -62,7 +62,7 @@ ContaminationQuantification <- function(object,cont_genes,
                                         min.cell = 50,
                                         top10_gcg = F,
                                         slot = 'counts'){
-  message('Calculating contamination level index...')
+  message('Calculating contamination ratio...')
   object <- NormalizeData(object, normalization.method = "LogNormalize", 
                           scale.factor = 10000, verbose = F) # normalization
   num_cells<-table(Idents(object))
@@ -79,10 +79,10 @@ ContaminationQuantification <- function(object,cont_genes,
     index<-Cal_Cont_level(object,x,eGCG_aucs[[1]],auc_thres = auc_thres,slot)
     return(index)
   }))
-  if (max(index_vals)>-7){
-    message(paste0('The maximum log contamination ratio is:',round(max(index_vals),2),', which indicates high contamination in this dataset. scCDC is highly recommended to be applied.'))
+  if (max(index_vals)>0.0003){
+    message(paste0('The maximum contamination ratio is:',round(max(index_vals),2),', which indicates high contamination in this dataset. scCDC is highly recommended to be applied.'))
   }else{
-    message(paste0('The maximum log contamination ratio is:',round(max(index_vals),2),', which indicates low contamination in this dataset. Either DecontX or scCDC is recommended to be applied.'))
+    message(paste0('The maximum contamination ratio is:',round(max(index_vals),2),', which indicates low contamination in this dataset. Either DecontX or scCDC is recommended to be applied.'))
   }
   return(max(index_vals))
 }
